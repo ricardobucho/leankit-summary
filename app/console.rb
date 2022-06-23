c258b170d98429a84e6219f09d071a5008e98621
Ricardo Bucho <ricardobucho@live.com>
2022-06-23 16:29:15 +0100

    Add console executable

---
+++ app/console.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative '../app/_autoload'
+
+# rubocop:disable Lint/Debugger
+binding.pry
+# rubocop:enable Lint/Debugger
--- app/pry.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require_relative '_autoload'
-
-binding.pry
